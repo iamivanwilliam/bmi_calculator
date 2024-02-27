59e52835df6207d7065a2ad9d9e40b88d3588356
ivan <ivan.dall@gmail.com>
2024-02-27 22:33:57 +0100

    A simple bmi calculator

---
+++ bmi.py
@@ -0,0 +1,9 @@
+# 1st input:
+height = input("Please input your height in meters (e.g: 1.70)\n")
+# 2nd input:
+weight = input("Please input your weight in kilograms (e.g: 72)\n")
+
+# calculation
+bmi = float(weight) / (float(height) ** 2)
+bmi_as_int = int(bmi)
+print("Your BMI is: " + str(bmi_as_int) + " and classifies as normal weight")
